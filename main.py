5322f3a2a5c840d743de252a858cd2202872a578
julienperce <58319391+julienperce@users.noreply.github.com>
2020-09-27 18:44:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ print("Welcome to the standard deviation calculator!")
 dataSet = list(map(float,input("Enter your dataset, seperated by spaces. Press enter when done: ").strip().split()))
 print(dataSet, len(dataSet))
 
-def average(dataSet):
-    dataSetNum = 0
+def average(dataSet): # the average of the dataset
+    dataSetNum = 0 
     for i in range (0, len(dataSet)):
         dataSetNum += dataSet[i]
     return float(dataSetNum / len(dataSet))
 
-def epsilon(dataSet):
+def epsilon(dataSet): 
     bases = []
     for i in range (0, len(dataSet)):
         bases.append(((dataSet[i] - average(dataSet))**2))
