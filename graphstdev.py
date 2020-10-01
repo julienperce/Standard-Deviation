3f4f27dcab40fab462205a9cd5af062b9563a9e9
Julien Perce <58319391+julienperce@users.noreply.github.com>
2020-10-01 16:34:37 -0500

    Update graphstdev.py

--- graphstdev.py
+++ graphstdev.py
@@ -23,13 +23,24 @@ for i in range (0, len(calculated)):
 
 print(f'The rejected values were {rejected}')
 
+for i in range (0, len(accepted)):
+    1s = []
+    2s = []
+    3s = []
+    if accepted[i] <= stdev:
+        1s.append(accepted[i])
+    elif accepted [i] >= stdev and accepted[i] <= (2 * stDev) and accepted[i] <= (-2 * stDev):
+        2s.append(accepted[i])
+    elif accepted [i] >= stdev and accepted[i] <= (3 * stDev) and accepted[i] <= (-3 * stDev):
+        3s.append(accepted[i])
+
 negXAxis = ((-3 * stDev)) # xMin
 posXAxis = ((3 * stDev)) # xMax
 negYAxis = (0) # yMin
 posYAxis = (100) # yMax
 
 
-plt.title("Standard deviation graphed to a 99.99% accuracy")
+plt.title("Standard Deviation graphed to a 99.99% accuracy")
 plt.axis([negXAxis, posXAxis, negYAxis, posYAxis])
 plt.xlabel("[-3σ;  3σ]")
 
