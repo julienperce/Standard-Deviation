b9ebac8f4bf12e7a282b86006a710525966e9a30
julienperce <58319391+julienperce@users.noreply.github.com>
2020-10-05 22:47:55 -0500

    Update graphstdev.py

--- graphstdev.py
+++ graphstdev.py
@@ -1,14 +1,17 @@
 import main
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+from scipy.stats import norm
 
 
 dataSet = main.dataSet
-average = main.average(main.dataSet)  
-stDev = main.stdev  
+average = main.average(main.dataSet)  # mean
+stDev = main.stdev # sigma
 calculated = [] # list that will get appended with (dataset[i] - stDev)
 accepted = []
 rejected = []
+confidenceLevel = 99.7
 
 for i in range (0, len(dataSet)):
     calcMeanDev = float(dataSet[i] - stDev)
@@ -23,27 +26,37 @@ for i in range (0, len(calculated)):
 
 print(f'The rejected values were {rejected}')
 
-for i in range (0, len(accepted)):
-    sigma1 = []  #in range (-stdev; stdev)
-    sigma2 = []  #in range (-2stdev; 2stdev)
-    sigma3 = []  #in range (-3stdev; 3stdev)   
-    if accepted[i] <= stDev:
-        sigma1.append(accepted[i])
-    elif accepted [i] >= stDev and accepted[i] <= (2 * stDev) and accepted[i] <= (-2 * stDev):
-        sigma2.append(accepted[i])
-    elif accepted [i] >= stDev and accepted[i] <= (3 * stDev) and accepted[i] <= (-3 * stDev):
-        sigma3.append(accepted[i])
+def marginOfError(stDev, confidenceLevel, calculated):
+    standardError = (stDev / (math.sqrt(len(calculated))))
+    errorMargin = standardError * confidenceLevel
+    return(errorMargin)   
 
-negXAxis = ((-3 * stDev)) # xMin
-posXAxis = ((3 * stDev)) # xMax
-negYAxis = (0) # yMin
-posYAxis = (100) # yMax
+lower_bound = average - marginOfError(stDev, confidenceLevel, calculated) 
+upper_bound = average + marginOfError(stDev, confidenceLevel, calculated) 
+
+z1 = (lower_bound - average) / stDev # -S (neg z transform)
+z2 = (upper_bound - average) / stDev # +s (pos z transform)
+
+x = np.arange(z1, z2, 0.001) # all numbers between z1 and z2 (incremented by 0.001)
+x_all = np.arange(-10, 10, 0.001) # all between -10 and 10 
+
+y = norm.pdf(x_all, 0.1)
 
 
-plt.title("Standard Deviation graphed to a 99.99% accuracy")
-plt.axis([negXAxis, posXAxis, negYAxis, posYAxis])
-plt.xlabel("[-3σ;  3σ]")
+
+
+
+fig, ax = plt.subplots(figsize=(9,6))
+plt.style.use('ggplot')
+ax.plot(x_all,y)
+
+plt.title("Standard Deviation graphed to a 99.7% accuracy", fontsize = 17)
+plt.xlim(-4, 4)
+plt.xlabel("[-3σ;  3σ]", fontsize = 14)
 
 print(f'The accepted values were {accepted}')
-plt.hist(accepted)
-plt.show()
+print(f"Margin of error: {marginOfError(stDev, confidenceLevel, calculated)}")
+print(f"Lower bound = {lower_bound} ; Upper bound = {upper_bound}")
+with plt.style.context('dark_background'):
+    ax.plot(x_all,y)
+    plt.show()
